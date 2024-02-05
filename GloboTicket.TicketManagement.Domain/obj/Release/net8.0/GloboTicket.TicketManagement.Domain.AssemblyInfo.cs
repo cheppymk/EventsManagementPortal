fa82daa3008a0b40ad28cd16b797534970b03a20
cheppymk <toso.micevski@gmail.com>
2024-02-06 00:07:52 +0100

    setting up the Application core

--- GloboTicket.TicketManagement.Domain/obj/Release/net8.0/GloboTicket.TicketManagement.Domain.AssemblyInfo.cs
+++ GloboTicket.TicketManagement.Domain/obj/Release/net8.0/GloboTicket.TicketManagement.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GloboTicket.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c977d6320cc4aae068d4a6fdba9db25ffb65bf10")]
 [assembly: System.Reflection.AssemblyProductAttribute("GloboTicket.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GloboTicket.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
